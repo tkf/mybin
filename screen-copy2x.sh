a82b4c1df7d431ea85990542c8cdd144e8728420
Takafumi ARAKAKI <takafumi@takafumi-bdc2.(none)>
2009-04-12 07:56:57 +0900

    screen copy to x

--- screen-copy2x.sh
+++ screen-copy2x.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-TMP=/tmp/screen-clipboard.`who am i | awk '{print $1}'`
+TMP=/tmp/screen-clipboard.`whoami | awk '{print $1}'`
 
-screen -X writebuf -e UTF-8 ${TMP}
+screen -X writebuf ${TMP}
 xclip ${TMP} -selection primary
 xclip ${TMP} -selection clipboard
 screen -X eval "echo \"buffer send to X clipboard\"" 
